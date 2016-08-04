6bbf0d0268b954f245a53fc825cd1c8afcfdb92d
chops318 <chops318@gmail.com>
2016-08-04 13:21:29 -0700

    added process.env

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.all('*', (req, res) => {
   res.status(404).send('not found');
 });
 
-const server = app.listen(3000, () => console.log('server up on 3000'));
+const server = app.listen(process.env.PORT || 3000, () => console.log('server up on 3000'));
 
 module.exports = server;
